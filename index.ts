5db88005888407ae71a54808bee455a6708794ba
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-04-23 17:18:42 +0200

    Fix lint errors and add CI

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
-const chatHistory: Array<{role: string; content: string}> = [
-//{role: 'system', content: 'You are Mario from Super Mario Bros. Answer as Mario, the assistant, only.'}
+type Message = {
+	role: string;
+	content: string;
+};
+
+const chatHistory: Message[] = [
+	//{role: 'system', content: 'You are Mario from Super Mario Bros. Answer as Mario, the assistant, only.'}
 ];
-while (true) {
+
+do {
 	const msg = prompt('=== You:');
 	if (!msg) {
-		break; 
+		break;
 	}
 
 	chatHistory.push({
@@ -12,18 +18,21 @@ while (true) {
 		content: msg,
 	});
 
-	const resp = await fetch('http://localhost:11434/api/chat', {method: 'POST', body: JSON.stringify({
-		model: 'llama3',
-		messages:chatHistory,
-		stream: false,
-	})});
+	const resp = await fetch('http://localhost:11434/api/chat', {
+		method: 'POST', body: JSON.stringify({
+			model: 'llama3',
+			messages: chatHistory,
+			stream: false,
+		}),
+	});
 	if (!resp.ok) {
-		break; 
+		break;
 	}
 
-	const answer = await resp.json();
+	const answer = await resp.json() as {message: Message};
 	chatHistory.push(answer.message);
 	console.log(`=== Bot: ${answer.message.content}`);
 	console.log();
 
-}
+// eslint-disable-next-line no-constant-condition
+} while (true);
